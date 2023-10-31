a3c24b9bb26a6bba91c3f5347012135a25e04819
mouhcine wildane <muhssenwildane@gmail.com>
2023-10-31 07:25:45 -0700

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i)=='q' or chr(i)=='e':
+    if chr(i) == 'q' or chr(i) == 'e':
         continue
     else:
         print('{}'.format(chr(i)), end='')

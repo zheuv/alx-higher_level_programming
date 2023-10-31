90d43e730122e57260acaced372dc400b87420f3
zheuv <muhssenwildane@gmail.com>
2023-10-31 17:42:00 +0100

    12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(1, 101):
+        c = i
+        if i % 3 == 0 and i % 5 == 0:
+            c = 'FizzBuzz'
+        elif i % 3 == 0:
+            c = 'Fizz'
+        elif i % 5 == 0:
+            c = 'Buzz'
+        print("{}".format(c), end = ' ')

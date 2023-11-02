d8f836625a47d23b1977f7efb592e43dbb0021c8
mouhcine wildane <muhssenwildane@gmail.com>
2023-11-02 09:38:27 -0700

    3tested

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,10 @@
 
 if __name__ == '__main__':
     import sys
-    
+
     count = len(sys.argv) - 1
     result = 0
-    
+
     for i in range(count):
         result = result + int(sys.argv[i+1])
 

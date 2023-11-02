468b2df561017c806c757f53a007f671bdb2d8b1
zheuv <muhssenwildane@gmail.com>
2023-11-02 17:34:50 +0100

    3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+    
+    count = len(sys.argv) - 1
+    result = 0
+    
+    for i in range(count):
+        result = result + int(sys.argv[i+1])
+
+    print(result)

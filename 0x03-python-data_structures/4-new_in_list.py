53a0c6235f03c20c8f120de4213f22a007ba07a9
zheuv <muhssenwildane@gmail.com>
2023-11-06 18:14:40 +0100

    1234

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    newList = list(mylist)
+    newList = []
+    for e in my_list:
+        newList.append(e)
 
     if idx < 0 or idx >= len(my_list):
         return my_list

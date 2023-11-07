8a74c10bfad7182aad35b3030439befa373fbf69
zheuv <muhssenwildane@gmail.com>
2023-11-07 22:25:59 +0100

    13

--- 0x03-python-data_structures/main.c
+++ 0x03-python-data_structures/main.c
@@ -13,12 +13,6 @@ int main(void)
 
     head = NULL;
     add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 17);
     add_nodeint_end(&head, 1);
     print_listint(head);
 

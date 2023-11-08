01f3fd97576c5b87ce53f8f9c117e2a9fbda5de4
zheuv <muhssenwildane@gmail.com>
2023-11-08 17:32:41 +0100

    123456789101112

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 def roman_to_int(roman_string):
     if isinstance(roman_string, str):
         
-        my_dict = {I:1,	V:5, X:10, L:50, C:100,	D:500,	M:1000}
+        my_dict = {'I':1,'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         result = 0
         
         for i in range(0,len(roman_string)):

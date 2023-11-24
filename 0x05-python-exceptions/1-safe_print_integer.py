d962451c84141a7274c6ab0f6e1bad8be17ce8eb
mouhcine wildane <muhssenwildane@gmail.com>
2023-11-24 18:34:27 +0100

    1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def printint(integer):
+def safe_print_integer(value):
     try:
-        print("{:d}".format(integer))
+        print("{:d}".format(value))
 
     except ValueError:
         return False

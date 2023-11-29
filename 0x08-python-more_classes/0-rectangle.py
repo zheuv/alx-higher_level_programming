f4055f6237b0667f77c4f1b19e05236c98a6a832
mouhcine wildane <muhssenwildane@gmail.com>
2023-11-29 16:42:27 +0100

    0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Module: geometry
+
+This module defines classes for basic geometric shapes.
+
+Classes:
+    Rectangle: Represents a rectangle.
+
+"""
+
+class Rectangle:
+    """
+    A class to represent a rectangle.
+
+    Attributes:
+        (None at the moment)
+    """
+
+    pass
+

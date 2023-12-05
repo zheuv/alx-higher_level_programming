6e5addbf577739658eefdae08966ca570ffd5e3d
mouhcine wildane <muhssenwildane@gmail.com>
2023-12-05 21:54:39 +0100

    6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,7 +7,6 @@ import json
 
 
 def load_from_json_file(filename):
-    """crate and object from a "JSON file" """    
-    with open(filename, 'r', encoding="utf_8") as f
+    """crate and object from a "JSON file" """
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
-

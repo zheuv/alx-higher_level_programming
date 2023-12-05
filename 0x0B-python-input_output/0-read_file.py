c4b8a12d53635066088cc1cb8ca30cd26c26fd5b
mouhcine wildane <muhssenwildane@gmail.com>
2023-12-05 20:51:39 +0100

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+"""
+Contains the read_file function
+"""
+
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding = "utf-8") as f:
-        for line in f:
-            print(line, end='')
-
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+            for line in f:
+                print(line, end='')

f9a1d6efc70536a8087a63aebc307cc8d8f53022
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-18 16:59:08 +0100

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,7 +5,7 @@ SELECT
 	tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON 
-	tv_shows.id = tv_shows_genres.show_id
+	tv_shows.id = tv_show_genres.show_id
 ORDER BY
 	tv_shows.title,
 	tv_show_genres.genre_id

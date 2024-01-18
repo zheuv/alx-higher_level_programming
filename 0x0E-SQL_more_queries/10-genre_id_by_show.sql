9c71f731ed4b60e135dbbe9b5864a62bde31ca63
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-18 16:57:09 +0100

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -7,4 +7,6 @@ SELECT
 FROM tv_shows
 JOIN tv_show_genres ON
 	tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.titles AND tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.titles,
+	tv_show_genres.genre_id 
+	ASC;

1ab1d614516e48ac36e6d925d3c3c0c0426ce881
mouhcine wildane <muhssenwildane@gmail.com>
2024-02-16 21:47:36 +0100

    9 to the factory

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" this script prints the states using ORM """
+from model_state import Base, State
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(State.name.like('%a%')).all():
+        print("{}: {}".format(state.id, state.name))
+    engine.dispose()

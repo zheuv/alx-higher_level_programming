bacf04bb8097d58e38f7dfc382d8a22840edba09
mouhcine wildane <muhssenwildane@gmail.com>
2024-02-16 14:46:01 +0100

    1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,16 @@
 """ a Python script that lists the states in the hbtn_0e_0_usa db """
 
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-import MySQLdb
-import sys
+    connection = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
 
-connection = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = connection.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id")
-rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-for row in rows:
-    print(row)
-
-cur.close()
-connection.close()
+    cur.close()
+    connection.close()

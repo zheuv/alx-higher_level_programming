7f53c715153b7e59d9a99bd4124885cadf30a79f
Mouhcine wildane <muhssenwildane@gmail.com>
2024-02-16 08:05:19 -0800

    5 format output

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,6 @@ if __name__ == "__main__":
             "JOIN states ON cities.state_id = states.id "
             "ORDER BY cities.id")
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
 
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-""" a Python script that lists the states in the hbtn_0e_0_usa db """
+""" A Python script that lists the cities in the hbtn_0e_0_usa db """
 
 if __name__ == "__main__":
     import MySQLdb
     import sys
 
+    # Establish connection to the database
     connection = MySQLdb.connect(
-            host="localhost", port=3306,
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        host="localhost", port=3306,
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+    )
     cur = connection.cursor()
+
+    # Execute SQL query to select cities
     cur.execute(
-    "SELECT cities.name FROM cities "
-    "JOIN (SELECT states.id AS idd FROM "
-    "states WHERE states.name LIKE BINARY %s) AS f "
-    "ON cities.state_id = f.idd", (sys.argv[4],))
+        "SELECT cities.name FROM cities "
+        "JOIN (SELECT states.id AS idd FROM "
+        "states WHERE states.name LIKE BINARY %s) AS f "
+        "ON cities.state_id = f.idd", (sys.argv[4],)
+    )
     rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+    # Concatenate city names into a single string separated by commas
+    city_names = ', '.join(row[0] for row in rows)
 
+    # Print the concatenated city names
+    print(city_names)
+
+    # Close cursor and connection
     cur.close()
     connection.close()

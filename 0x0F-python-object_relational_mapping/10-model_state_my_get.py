d976a3db1b1beb9dc3724107444297d7872a5d60
mouhcine wildane <muhssenwildane@gmail.com>
2024-02-16 14:29:13 -0800

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ this script prints the id of the name the user gave """
-
+from sqlalchemy import func
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
@@ -15,9 +15,10 @@ if __name__ == "__main__":
     )
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name.like.binary(sys.argv[4])).first()
+    instance = session.query(State).filter(func.binary(State.name).like('%{}%'.format(sys.argv[4]))).first()
     if instance:
         print(instance.id)
     else:
         print("Not found")
     engine.dispose()
+

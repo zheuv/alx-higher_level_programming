8eb5329cd598d9575d2c809d533f0d8d16a7f932
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-02-16 15:54:03 +0100

    Create 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" a python script that lists the states starting with N in ascending order
+    from the hbtn_0e_0_usa db """
+if __name__ == "__main__":
+    import MySQLdb as db
+    import sys
+
+    connection = db.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = connection.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE "
+        "states.name LIKE BINARY %s ORDER BY states.id", (sys.argv[4],))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    connection.close()

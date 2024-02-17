bd71e5611267e4eca6641a8bb8793ffc44858d8f
mouhcine wildane <muhssenwildane@gmail.com>
2024-02-17 22:41:07 +0100

    14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3 
+""" this script lists the cities in the cities table """
+
+from model_city import City
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instances = session.query(City, State).\
+            join(State, State.id == City.state_id).all()
+    if instances:
+        for city, state in instances:
+            print(f"{state.name}: ({city.id}) {city.name}")
+    engine.dispose()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this script is the city model class """
+from from sqlalchemy import Column, String, Integer
+from model_state import Base
+
+class City(Base):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))

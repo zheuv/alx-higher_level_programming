84831a5a728452979dfede5a06ea33cf09bcc170
mouhcine wildane <muhssenwildane@gmail.com>
2024-03-01 12:13:21 -0800

    chmod

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+""" Write a Python script that takes your GitHub credentials
+    (username and password) and uses the GitHub API to display your id """
 
 if __name__ == "__main__":
     import requests

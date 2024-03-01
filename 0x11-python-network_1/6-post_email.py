91b2c54451a531db755e0f01692198116ddbefdb
mouhcine wildane <muhssenwildane@gmail.com>
2024-03-01 11:00:33 -0800

    chmod

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,7 +3,7 @@
     sends a POST request to the passed URL with the email as a parameter,
     and finally displays the body of the response. """
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     import requests
     import sys
 

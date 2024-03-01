d297ca9a5f968161b98d2e9218b2b64c3358d5d5
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-03-01 20:01:05 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Python script that takes in a URL,
-    sends a request to the URL and displays the value of 
+    sends a request to the URL and displays the value of
     the variable X-Request-Id in the response header """
 
 if __name__ == "__main__":

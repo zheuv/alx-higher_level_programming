ddb7ca65ef1f12bde1f6b00bab67bb2df7acdeca
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-03-01 17:46:53 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({email:email}).encode('utf-8')
+    data = urllib.parse.urlencode({email: email}).encode('utf-8')
     req = urllib.request.Request(url, data=data, method='POST')
 
     with urllib.request.urlopen(req) as response:

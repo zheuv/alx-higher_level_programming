a888807f4b9c5f1550e89d71de0ff80f410fa854
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-03-01 20:01:28 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Write a Python script that 
+""" Write a Python script that
     takes in a letter and sends a POST request to
     http://0.0.0.0:5000/search_user with the letter as a parameter.
     handles the json response"""

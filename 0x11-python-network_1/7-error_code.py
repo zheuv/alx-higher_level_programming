12e484928785a2ec33e123210817889ee6cdcea5
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-03-02 14:48:38 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
     if response.status_code >= 400:
-        print("Error code: ", response.status_code)
+        print("Error code:", response.status_code)
         exit()
     print(response.text)
